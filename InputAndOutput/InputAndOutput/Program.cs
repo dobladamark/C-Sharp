1c953c88614ecc274d45881869141fc9c169be28
mark <122426238+dobladamark@users.noreply.github.com>
2025-08-07 17:08:19 +0800

    Add files via upload

---
+++ InputAndOutput/InputAndOutput/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your age : ");
+        string input = Console.ReadLine();
+
+        int age = int.Parse(input);
+
+        Console.WriteLine("Your age is: " + age + " years old.");
+        
+    }
+}

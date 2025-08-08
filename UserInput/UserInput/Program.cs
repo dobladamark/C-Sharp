b5bdb3e0c54de6ebc01f1368f8d795e2e5503a62
mark <122426238+dobladamark@users.noreply.github.com>
2025-08-08 14:30:56 +0800

    Add files via upload

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter username: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Username is: " + username);
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+
+        }
+    }
+}
